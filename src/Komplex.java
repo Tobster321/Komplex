89de5cb9f1140b373b406304aea154fae25c1caa
tobias.sandstrom1 <tobias.sandstrom@elev.ga.ntig.se>
2020-02-07 11:14:45 +0100

    add test

--- src/Komplex.java
+++ src/Komplex.java
@@ -9,22 +9,25 @@ public class Komplex {
 
     @Override
     public String toString() {
-        return "Komplex{" +
-                "im=" + im +
-                ", re=" + re +
-                '}';
+        return "Komplex{" + re + " + " + im + "i}";
     }
 
     public static void main(String[] args) {
-        Komplex a = new Komplex(3, 8);
-        Komplex b = new Komplex(4, 7);
+        Komplex a = new Komplex(8, 3);
+        Komplex b = new Komplex(7, 4);
         Komplex c = a.add(b);
-        System.out.println(c);
+        System.out.println("Talet borde vara 15 + 7i:\n" + c);
     }
 
-    private Komplex add(Komplex b) {
-        return new Komplex(d);
+    public double getIm() {
+        return im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+   private Komplex add(Komplex b) {
+       return new Komplex(this.re + b.re, this.im + b.im);
     }
 }
-
-
